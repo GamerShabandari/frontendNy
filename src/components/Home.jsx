a20b5baf20ff6a651f52fe8023a18f86738d5ebc
GamerSh <93838417+GamerShabandari@users.noreply.github.com>
2022-08-30 15:44:15 +0200

    test

---
+++ src/components/Home.jsx
@@ -0,0 +1,59 @@
+import { useEffect, useState } from "react";
+import { io } from "socket.io-client"
+export const socket = io('http://localhost:3001', { "autoConnect": false })
+
+export function Home() {
+
+    const [username, setUsername] = useState("");
+    const [roomToJoin, setRoomToJoin] = useState("");
+    const [availableRooms, setAvailableRooms] = useState([]);
+
+    useEffect(() => {
+        socket.connect();
+
+    }, []);
+
+
+
+      socket.on("availableRooms", function (rooms) {
+        setAvailableRooms([...rooms])
+        console.log(rooms)
+      });
+
+
+    function join() {
+        let user = {
+            nickname: username
+        }
+        socket.emit("joinNewRoom", roomToJoin, user);
+    }
+
+    function joinAvailableRoom(roomName){
+        let user = {
+            nickname: username
+        }
+        socket.emit("joinAvailableRoom", roomName, user);
+    }
+
+    let roomsHTML =  availableRooms.map((room, i) =>{
+        return(
+            <div key={i}>
+                <h3 onClick={()=>{joinAvailableRoom(room.roomName)}}>{room.roomName}</h3>
+
+            </div>
+        )
+    })
+
+    return (<>
+        <h1>hej och välkommen {username}</h1>
+        <input type="text" placeholder="nickname" onChange={(e) => { setUsername(e.target.value) }} />
+        <input type="text" placeholder="room" onChange={(e) => { setRoomToJoin(e.target.value) }} />
+        <button onClick={join}>join</button>
+
+        <div>{username} - {roomToJoin}</div>
+        {availableRooms.length}
+        {roomsHTML}
+      
+    </>)
+
+}
